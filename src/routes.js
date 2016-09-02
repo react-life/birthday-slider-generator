a27fc6715d66bc49a2a8b1b08c1c5504e6b8d0a3
Sergey Karinsky <reactlife.js@gmail.com>
2016-09-03 02:28:23 +0700

    Setup api

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,6 @@ import Login from 'containers/Login';
 import RecoveryPassword from 'containers/RecoveryPassword';
 import Signup from 'containers/Signup';
 
-import Layout from 'containers/Layout';
-
-import User from 'containers/User';
 import Institution from 'containers/Institution';
 import Staff from 'containers/Staff';
 import Roles from 'containers/Roles';
@@ -29,19 +26,6 @@ export default (
         <Route path='recovery' component={RecoveryPassword} />
       </Route>
       <Route path='/signup' component={Signup} />
-    </Route>
-
-    <Route component={Layout}>
-      <Route path='/user' component={User}>
-        <Route path='institution' component={Institution} />
-        <Route path='staff' component={Staff} />
-        <Route path='roles' component={Roles} />
-        <Route path='devices' component={Devices} />
-        <Route path='journal' component={Journal} />
-        <Route path='profile' component={Profile}>
-          <Route path='change-password' component={ChangePassword} />
-        </Route>
-      </Route>
     </Route>
 
     <Route path='*' component={NotFound} status={404} />
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import reactCookie from 'react-cookie';
+import httpProxy from 'http-proxy';
 import path from 'path';
 import http from 'http';
 import webpack from 'webpack';
@@ -16,10 +17,44 @@ import Html from './helpers/Html';
 const app = new Express();
 const server = new http.Server(app);
 
+const targetUrl = 'http://' + config.apiHost + ':' + config.apiPort;
+const proxy = httpProxy.createProxyServer({
+  target: targetUrl,
+  ws: true
+});
+
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(session({ secret: 'keyboard', resave: false, saveUninitialized: false }));
+
+
+// Proxy to API server
+app.use('/api', (req, res) => {
+  proxy.web(req, res, {target: targetUrl});
+});
+
+app.use('/ws', (req, res) => {
+  proxy.web(req, res, {target: targetUrl + '/ws'});
+});
+
+server.on('upgrade', (req, socket, head) => {
+  proxy.ws(req, socket, head);
+});
+
+// added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
+proxy.on('error', (error, req, res) => {
+  let json;
+  if (error.code !== 'ECONNRESET') {
+    console.error('proxy error', error);
+  }
+  if (!res.headersSent) {
+    res.writeHead(500, {'content-type': 'application/json'});
+  }
+
+  json = {error: 'proxy_error', reason: error.message};
+  res.end(JSON.stringify(json));
+});
 
 app.use(Express.static(path.join(__dirname, '..', 'static')));
 
